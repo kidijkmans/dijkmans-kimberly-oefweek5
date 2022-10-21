1325c1325cdd09b47949db4c6bb1d52b142db3e6
Kimberly Dijkmans <kim.14420@outlook.com>
2022-10-21 18:24:41 +0100

    changed port

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.set('landenFile', require('./data/landen.json'));
 
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 8000));
 app.use(express.static('public'));
 
 app.use(require("./routes/root_router"));
